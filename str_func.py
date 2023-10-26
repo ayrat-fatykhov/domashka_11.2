38e7d172a6866146a1aeb4904fae5948526991bc
Ayrat Fatykhov <airat_91_91@mail.ru>
2023-10-26 20:37:10 +0300

    add functiоn get_title

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def foo(line):
     """Функция делает все буквы заглавными"""
     return line.upper()
+
+
+def get_title(line):
+    """Функция делает первую букву каждого слова заглавной"""
+    return line.title()
